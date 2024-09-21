3d77036d5bfe3b214c4cea9e469fd18691d57c91
Arunav Bhattacharya <itsarunav4u@gmail.com>
2024-09-20 22:18:09 -0700

    Gradle Build Files updated

---
+++ core/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "me.arunav.modules"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
